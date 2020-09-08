50b942869a07bd196d5890403a5e245c614bd50e
Alvin Escobar <alvincsproductions@gmail.com>
2020-09-08 17:55:17 -0500

    singleton setup

---
+++ AlvinSwagerLine/Model/Category.swift
@@ -0,0 +1,19 @@
+//
+//  Category.swift
+//  AlvinSwagerLine
+//
+//  Created by Alvin Escobar on 9/8/20.
+//  Copyright © 2020 Alvin Escobar. All rights reserved.
+//
+
+import Foundation
+
+struct Category {
+    private(set) public var title: String
+    private(set) public var imageName: String
+    
+    init(title: String, imageName: String) {
+        self.title = title
+        self.imageName = imageName
+    }
+}
---
+++ AlvinSwagerLine/Services/DataService.swift
@@ -0,0 +1,27 @@
+//
+//  DataService.swift
+//  AlvinSwagerLine
+//
+//  Created by Alvin Escobar on 9/8/20.
+//  Copyright © 2020 Alvin Escobar. All rights reserved.
+//
+
+import Foundation
+
+// Create a singleton design pattern
+
+class DataService {
+    static let instance = DataService()
+    
+    private let categories: [Category] = [
+        Category(title: "Laptops", imageName: "computer.png"),
+        Category(title: "Chips", imageName: "chips.png"),
+        Category(title: "Keyboard", imageName: "keyboard.png")
+    ]
+    
+    
+    // this function would act as if it goes to get data from a server
+    func getCategories() -> [Category] {
+        return categories
+    }
+}
